193c7f3b9d6762be121e5042acdfb37d535ebc22
Gustav <gustavkanahols@gmail.com>
2022-03-09 11:25:32 +0100

    CheckCollision() i Snake Kollar om huvudet av en Snake kolliderar med ett snake segment. ReferenceEquals bör lösa specialfallet som uppstår om den kollar kollision med sig själv eftersom en snake inte ska kunna kollidera med sitt egna huvud. OBS detta är otestat än så länge eftersom vi de delarna som krävs för att testa det inte har skrivits än.

--- ProjectSnake/Snake.cs
+++ ProjectSnake/Snake.cs
@@ -1,5 +1,7 @@
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace ProjectSnake
 {
@@ -33,6 +35,18 @@ namespace ProjectSnake
         public void OnCollision(Player player)
         {
             throw new System.NotImplementedException();
+        }
+
+        // Returns true if a snake's head collides with another snake segment
+        public bool CheckCollision(Snake snake)
+        {
+            // Special case when checking collisions between a snake and itself to avoid colliding with it's own head
+            if (ReferenceEquals(this, snake))
+            {
+                return snake.segments.Skip(1).Any(segment => (segments[0].X, segments[0].Y) == (segment.X, segment.Y));
+            }
+            // If colliding with another snake
+            return snake.segments.Any(segment => (segments[0].X, segments[0].Y) == (segment.X, segment.Y));
         }
     }
 }
