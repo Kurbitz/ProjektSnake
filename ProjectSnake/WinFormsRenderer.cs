c6c6d0474833527387d6d510b1bf19e50da1491d
Johan Sandred <jsd21003@student.mdu.se>
2022-03-17 14:27:16 +0100

    Lägg till ett WinFormsRenderer-klass-skelett

---
+++ ProjectSnake/WinFormsRenderer.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectSnake
+{
+    public class WinFormsRenderer : IRenderer
+    {
+        public void Draw(Food food)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Draw(Snake snake)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
