a4f4e6006e1bdec8ed65d44748be66884ec5265f
Johan Sandred <jsd21003@student.mdu.se>
2022-03-18 14:39:59 +0100

    Lägg till ritningar för ormar i Player
    Varje orm behöver en unik startposition och en unik färg. Vi skapar en
    getter i Player som ger tillbaka en array med unika positioner och
    färger. I framtiden kanske vi vill göra så att färgerna kan väljas av
    spelarna eller att positionerna slumpas eller liknande, men för
    tillfället är det här ett lätt sätt att lösa det på.

--- ProjectSnake/Player.cs
+++ ProjectSnake/Player.cs
@@ -7,6 +7,12 @@ namespace ProjectSnake
         public Snake Snake { get; }
         private int score;
 
+        public static (PointF Position, Color Color)[] SnakeBlueprints { get; } =
+        {
+            (new PointF(1F / 3F, 1F / 3F), Color.Green), (new PointF(2F / 3F, 1F / 3F), Color.Black),
+            (new PointF(1F / 3F, 2F / 3F), Color.Brown)
+        };
+
         public Player(Point snakeStartingPos, Color snakeColor)
         {
             Snake = new Snake(snakeStartingPos, snakeColor);
