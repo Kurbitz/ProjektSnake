ed09ef36c16a86047b7052e9c6ae02276cf14881
Johan Sandred <jsd21003@student.mdu.se>
2022-03-18 14:06:17 +0100

    Skapa en konstruktor för Player

--- ProjectSnake/Player.cs
+++ ProjectSnake/Player.cs
@@ -1,8 +1,15 @@
-﻿namespace ProjectSnake
+﻿using System.Drawing;
+
+namespace ProjectSnake
 {
     public class Player
     {
         public Snake snake;
         private int score;
+
+        public Player(Point snakeStartingPos, Color snakeColor)
+        {
+            snake = new Snake(snakeStartingPos, snakeColor);
+        }
     }
 }
