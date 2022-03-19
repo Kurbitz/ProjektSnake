dd936804b452da5a1ec9aec7914598c4f1e8d0f0
Johan Sandred <jsd21003@student.mdu.se>
2022-03-19 16:21:17 +0100

    Lägg till en RotateOneStepRight extension method
    Metoden roterar varje element i en IList<T> ett steg åt höger.

---
+++ ProjectSnake/Algorithms.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ProjectSnake
+{
+    public static class Algorithms
+    {
+        // Roterar alla element i en lista ett steg åt höger.
+        // T.ex. [1, 2, 3, 4, 5] -> [5, 1, 2, 3, 4]
+        public static void RotateOneStepRight<T>(this IList<T> list)
+        {
+            var tmp = list.Last();
+            for (var i = list.Count - 2; i >= 0; --i)
+            {
+                list[i + 1] = list[i];
+            }
+
+            list[0] = tmp;
+        }
+    }
+}
