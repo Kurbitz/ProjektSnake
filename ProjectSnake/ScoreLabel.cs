46b621bb84f8270e28d0fd6f33ddb4409b8045f9
Johan Sandred <jsd21003@student.mdu.se>
2022-03-20 15:24:38 +0100

    Lägg till en ScoreLabel-klass
    Klassen är en Label som associerar sig till en Player så att den kan
    komma åt spelarens poäng.

---
+++ ProjectSnake/ScoreLabel.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace ProjectSnake
+{
+    public class ScoreLabel : Label
+    {
+        private readonly Player _player;
+
+        public ScoreLabel(Player player)
+        {
+            _player = player;
+            ForeColor = player.Snake.Color;
+        }
+
+        public void UpdateText()
+        {
+            Text = $"Score: {_player.Score}";
+        }
+    }
+}
