f85d35d58643ad64d2a32757887286a925d676e9
Gustav Kånåhols <gustavkanahols@gmail.com>
2022-03-22 15:51:42 +0100

    Lägg till ColorScheme struct
    Innehåller en färg för huvudet och en för kroppen

---
+++ ProjectSnake/SnakeColorScheme.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace ProjectSnake
+{
+    public struct SnakeColorScheme
+    {
+        public Color Head;
+        public Color Body;
+
+        public SnakeColorScheme(Color head, Color body)
+        {
+            Head = head;
+            Body = body;
+        }
+    }
+}
