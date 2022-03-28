a8ba59f1db22aed454737b491217e2097d45e9a7
Gustav Kånåhols <gustavkanahols@gmail.com>
2022-03-28 21:51:30 +0200

    Ny klass RandomizeControlsFood

---
+++ ProjectSnake/RandomizeControlsFood.cs
@@ -0,0 +1,17 @@
+﻿using System.Drawing;
+
+namespace ProjectSnake
+{
+    public class RandomizeControlsFood : Food
+    {
+        public RandomizeControlsFood(Point pos) : base(pos, Gruvbox.Purple, 10, 1)
+        {
+        }
+
+        public override void OnCollision(Player player)
+        {
+            player.OnCollision(this);
+            IsActive = false;
+        }
+    }
+}
