ba36a7b496b1b60863961014f499727a2ab38345
Gustav Kånåhols <gustavkanahols@gmail.com>
2022-03-28 21:52:23 +0200

    Ny controlscheme för spelare 3

--- ProjectSnake/Controls.cs
+++ ProjectSnake/Controls.cs
@@ -18,7 +18,12 @@ namespace ProjectSnake
             {Keys.A, Direction.Left}, {Keys.D, Direction.Right}, {Keys.W, Direction.Up}, {Keys.S, Direction.Down}
         };
 
-        public static readonly Controls[] ControlsBlueprints = {new Controls(Player1), new Controls(Player2)};
+        private static readonly Dictionary<Keys, Direction> Player3 = new Dictionary<Keys, Direction>()
+        {
+            {Keys.J, Direction.Left}, {Keys.L, Direction.Right}, {Keys.I, Direction.Up}, {Keys.K, Direction.Down}
+        };
+        
+        public static readonly Controls[] ControlsBlueprints = {new Controls(Player1), new Controls(Player2), new Controls(Player3)};
 
         private readonly Dictionary<Keys, Direction> _dictionary;
 
